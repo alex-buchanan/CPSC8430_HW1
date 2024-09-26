4b202fbcc3e1b08e7daac0ecfd25be66d5b161fc
alex-buchanan <alex.buchanan210@gmail.com>
2024-09-26 18:47:48 -0400

    All code done, graphs being updated

--- Flat_Gen_Cross.py
+++ Flat_Gen_Cross.py
@@ -23,7 +23,8 @@ test_data = datasets.MNIST(
     transform=ToTensor(),
 )
 
-batch_size = 64
+batch_size_1 = 64
+batch_size_2 = 1024
 
 # for X, y in test_dataloader:
 #     print(f"Shape of X [N, C, H, W]: {X.shape}")
@@ -41,7 +42,7 @@ print(f"Using {device} device")
 
 # Define model
 class NeuralNetwork(nn.Module):
-
+    '''
     def __init__(self, nInput, nHidden, nOutput):
         super(NeuralNetwork, self).__init__()
         self.nInput  = nInput
@@ -69,15 +70,39 @@ class NeuralNetwork(nn.Module):
         # h7 = self.ReLU(self.linear7(h5))
         output = self.linear4(h3) ## <- NOTE
         return(output)
+    '''
+    def __init__(self, nInput, nHidden, nOutput):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.nInput = nInput
+        self.nOutput = nOutput
+        self.nHidden = nHidden
 
-model = NeuralNetwork(784, 256, 10).to(device)
-print(model)
+        self.ReLU = nn.ReLU()
+        """225"""
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=16)
+        # self.conv2 = nn.Conv2d(32, 32, kernel_size=4)
+        self.linear1 = nn.Linear(5408, self.nOutput)
+
+
+    def forward(self, x):
+        # x = self.flatten(x)
+        # print(x.shape)
+        c1 = self.flatten(self.ReLU(self.conv1(x)))
+        # print(f"c1 weights: {c1.weights.data}")
+        # c2 = self.flatten(self.ReLU(self.conv2(c1)))
+        logits = self.linear1(c1)
+        return logits
+
+model_64 = NeuralNetwork(32, 256, 10).to(device)
+model_1024 = NeuralNetwork(32, 256, 10).to(device)
+model_agg = NeuralNetwork(32, 256, 10).to(device)
+print(model_1024)
 
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+optimizer_1 = torch.optim.SGD(model_64.parameters(), lr=1e-2)
+optimizer_2 = torch.optim.SGD(model_1024.parameters(), lr=1e-2)
 
-l_test = []
-l_train = []
 epoch = []
 '''
 class MyTwistedMNIST(torch.utils.data.Dataset):
@@ -95,14 +120,11 @@ class MyTwistedMNIST(torch.utils.data.Dataset):
   def __len__(self):
     return self.orig_mnist.__len__()
 '''
-def train(dataloader, model, loss_fn, optimizer):
+def train(dataloader, model, loss_fn, optimizer, l_train):
     size = len(training_data)
     model.train()
     train_loss = 0.0
     for batch, (X, y) in enumerate(dataloader):
-        if random.randrange(0, 1) > 0.5:
-            c = torch.randperm(len(y))
-            y = y[c]
         X, y = X.to(device), y.to(device)
 
         # Compute prediction error
@@ -134,7 +156,7 @@ def train(dataloader, model, loss_fn, optimizer):
     train_loss /= len(dataloader)
     l_train.append(train_loss)
 
-def test(dataloader, model, loss_fn):
+def test(dataloader, model, loss_fn, l_test):
     size = len(test_data)
     num_batches = len(dataloader)
     model.eval()
@@ -150,34 +172,112 @@ def test(dataloader, model, loss_fn):
     l_test.append(correct*100)
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-epochs = 100
+epochs = 20
 
 grad_plot = []
+l_test_1 = []
+l_train_1 = []
+l_test_2 = []
+l_train_2 = []
+theta_alpha_test_accy = []
+cross_entropy_test = []
+theta_alpha_train_accy = []
+cross_entropy_train = []
 
 
 for t in range(epochs):
+    
     print(f"Epoch {t+1}\n-------------------------------")
     epoch.append(t+1)
 
-    sub_size_train = list([random.randrange(0, len(training_data)) for a in range(len(training_data)//2)])
-    sub_size_test = list([random.randrange(0, len(test_data)) for a in range(len(test_data)//2)])
+    sub_size_train = list([random.randrange(0, len(training_data)) for a in range(len(training_data))])
+    sub_size_test = list([random.randrange(0, len(test_data)) for a in range(len(test_data))])
 
     train_subset = torch.utils.data.Subset(training_data, sub_size_train)
     test_subset = torch.utils.data.Subset(test_data, sub_size_test)
 
     # Create data loaders.
-    train_dataloader = DataLoader(train_subset, batch_size=batch_size, shuffle=True)
-    test_dataloader = DataLoader(test_subset, batch_size=batch_size)
+    train_dataloader_1 = DataLoader(train_subset, batch_size=batch_size_1, shuffle=True)
+    test_dataloader_1 = DataLoader(test_subset, batch_size=batch_size_1)
 
-    train(train_dataloader, model, loss_fn, optimizer)
+    train(train_dataloader_1, model_64, loss_fn, optimizer_1, l_train_1)
+    test(test_dataloader_1, model_64, loss_fn, l_test_1)
 
-    test(test_dataloader, model, loss_fn)
+    # Create data loaders.
+    train_dataloader_2 = DataLoader(train_subset, batch_size=batch_size_2, shuffle=True)
+    test_dataloader_2 = DataLoader(test_subset, batch_size=batch_size_2)
+
+    train(train_dataloader_2, model_1024, loss_fn, optimizer_2, l_train_2)
+    test(test_dataloader_2, model_1024, loss_fn, l_test_2)
 print("Done!")
+
+model_params = model_64.state_dict()
+
+fig1, ax1 = plt.subplots()
+
+for a in [(t/100.0)-1.0 for t in range(0,300)]:
+    state_1 = model_64.state_dict()
+    state_2 = model_1024.state_dict()
+    for layer in state_1:
+        model_params[layer] = (1-a)*state_1[layer] + a*state_2[layer]   
+    model_agg.load_state_dict(model_params)
+
+    train_loss, correct, cross_entropy = 0, 0, 0
+    for X, y in train_dataloader_2:
+        X, y = X.to(device), y.to(device)
+        pred = model_agg(X)
+        train_loss += loss_fn(pred, y).item()
+        cross_entropy += nn.functional.cross_entropy(pred,y).item()
+        correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    theta_alpha_train_accy.append(correct/len(training_data)*100)
+    cross_entropy_train.append(cross_entropy)
+
+    test_loss, correct, cross_entropy = 0, 0, 0
+    for X, y in test_dataloader_2:
+        X, y = X.to(device), y.to(device)
+        pred = model_agg(X)
+        test_loss += loss_fn(pred, y).item()
+        cross_entropy += nn.functional.cross_entropy(pred,y).item()
+        correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    theta_alpha_test_accy.append(correct/len(test_data)*100)
+    cross_entropy_test.append(cross_entropy)
+
+ax1.set_xlabel('$ \\alpha $')
+ax1.set_ylabel('Accuracy')
+ax1.plot([(t/100.0)-1.0 for t in range(0,300)], theta_alpha_train_accy, 'r')
+ax1.plot([(t/100.0)-1.0 for t in range(0,300)], theta_alpha_test_accy, 'r--')
+ax1.axis([-1, 2, 0, 100])
+
+ax2 = ax1.twinx()
+ax2.set_ylabel('Entropy')
+ax2.plot([(t/100.0)-1.0 for t in range(0,300)], cross_entropy_train, 'b')
+ax2.plot([(t/100.0)-1.0 for t in range(0,300)], cross_entropy_test, 'b--')
+ax2.set_yscale('log')
+ax2.axis([-1, 2, 0.001, 1000])
+
+fig1.suptitle('$ \\alpha $ Variation', fontsize=16)
+fig1.tight_layout()
+
+
 
 # plot the dataecurse=True
 plt.figure()
-plt.plot(range(epochs), l_test, color="r", alpha= 1.0, label= "Plt_Test")
-plt.plot(range(epochs), l_train, color="b", alpha= 1.0, label= "Plt_Train")
+plt.subplot(1,2,1)
+plt.plot(range(epochs), l_test_1, 'ro', label= "Plt_Test")
+plt.plot(range(epochs), l_train_1, 'bo', label= "Plt_Train")
+plt.axis([0, 20, 0, 100])
+plt.title('Batch = 64')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+
+plt.subplot(1,2,2)
+plt.plot(range(epochs), l_test_2, 'ro', alpha= 1.0, label= "Plt_Test")
+plt.plot(range(epochs), l_train_2, 'bo', alpha= 1.0, label= "Plt_Train")
+plt.axis([0, 20, 0, 100])
+plt.title('Batch = 1024')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+
 # plt.yscale('log')
 
 # fig, ax = plt.subplots()
