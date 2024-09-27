90cc70d882e94d9587824e1c44ba1c18458a5f12
alex-buchanan <alex.buchanan210@gmail.com>
2024-09-27 14:41:12 -0400

    Completed through HW2-2

--- GradientNorm_Linear.py
+++ GradientNorm_Linear.py
@@ -56,148 +56,6 @@ yObs = np.sin(5*np.pi*xObs)/(5*np.pi*xObs) # + yNoise
 # plt.show()
 
 ##################################################
-## network 1 dimension parameters
-##################################################
-
-nInput  = 1
-nHidden = 60
-nOutput = 1
-
-##################################################
-## set up multi-layer perceptron w/ PyTorch
-##    -- version 1 --
-##################################################
-'''
-class MLPexplicit_1(nn.Module):
-    
-    # Multi-layer perceptron for non-linear regression.
-    
-    def __init__(self, nInput, nHidden, nOutput):
-        super(MLPexplicit_1, self).__init__()
-        self.nInput  = nInput
-        self.nHidden = nHidden
-        self.nOutput = nOutput
-        self.linear1 = nn.Linear(self.nInput,  self.nHidden)
-        self.linear2 = nn.Linear(self.nHidden, self.nOutput)
-        # self.linear3 = nn.Linear(self.nHidden, self.nHidden)
-        # self.linear4 = nn.Linear(self.nHidden, self.nHidden)
-        # self.linear5 = nn.Linear(self.nHidden, self.nHidden)
-        # self.linear6 = nn.Linear(self.nHidden, self.nHidden)
-        # self.linear7 = nn.Linear(self.nHidden, self.nHidden)
-        # self.linear8 = nn.Linear(self.nHidden, self.nOutput)
-        self.ReLU    = nn.ReLU()
-
-    def forward(self, x):
-        h1 = self.ReLU(self.linear1(x))
-        # h2 = self.ReLU(self.linear2(h1))
-        # h3 = self.ReLU(self.linear3(h2))
-        # h4 = self.ReLU(self.linear4(h3))
-        # h5 = self.ReLU(self.linear5(h4))
-        # h6 = self.ReLU(self.linear6(h5))
-        # h7 = self.ReLU(self.linear7(h5))
-        output = self.linear2(h1) ## <- NOTE
-        return(output)
-
-mlpExplicit_1 = MLPexplicit_1(nInput, nHidden, nOutput)
-# which model to use from here onwards
-model_1 = mlpExplicit_1.to(device)
-
-##################################################
-## network 2 dimension parameters
-##################################################
-
-nInput  = 1
-nHidden = 30
-nOutput = 1
-
-##################################################
-## set up multi-layer perceptron w/ PyTorch
-##    -- version 1 --
-##################################################
-
-class MLPexplicit_2(nn.Module):
-    
-    # Multi-layer perceptron for non-linear regression.
-    
-    def __init__(self, nInput, nHidden, nOutput):
-        super(MLPexplicit_2, self).__init__()
-        self.nInput  = nInput
-        self.nHidden = nHidden
-        self.nOutput = nOutput
-        self.linear1 = nn.Linear(self.nInput,  self.nHidden)
-        self.linear2 = nn.Linear(self.nHidden, self.nHidden)
-        self.linear3 = nn.Linear(self.nHidden, self.nHidden)
-        self.linear4 = nn.Linear(self.nHidden, self.nOutput)
-        # self.linear5 = nn.Linear(self.nHidden, self.nHidden)
-        # self.linear6 = nn.Linear(self.nHidden, self.nHidden)
-        # self.linear7 = nn.Linear(self.nHidden, self.nHidden)
-        # self.linear8 = nn.Linear(self.nHidden, self.nOutput)
-        self.ReLU    = nn.ReLU()
-
-    def forward(self, x):
-        h1 = self.ReLU(self.linear1(x))
-        h2 = self.ReLU(self.linear2(h1))
-        h3 = self.ReLU(self.linear3(h2))
-        # h4 = self.ReLU(self.linear4(h3))
-        # h5 = self.ReLU(self.linear5(h4))
-        # h6 = self.ReLU(self.linear6(h5))
-        # h7 = self.ReLU(self.linear7(h5))
-        output = self.linear4(h3) ## <- NOTE
-        return(output)
-
-mlpExplicit_2 = MLPexplicit_2(nInput, nHidden, nOutput)
-# which model to use from here onwards
-model_2 = mlpExplicit_2.to(device)
-
-##################################################
-## network 1 dimension parameters
-##################################################
-
-nInput  = 1
-nHidden = 20
-nOutput = 1
-
-##################################################
-## set up multi-layer perceptron w/ PyTorch
-##    -- version 1 --
-##################################################
-
-class MLPexplicit_3(nn.Module):
-    
-    # Multi-layer perceptron for non-linear regression.
-    
-    def __init__(self, nInput, nHidden, nOutput):
-        super(MLPexplicit_3, self).__init__()
-        self.nInput  = nInput
-        self.nHidden = nHidden
-        self.nOutput = nOutput
-        self.linear1 = nn.Linear(self.nInput,  self.nHidden)
-        self.linear2 = nn.Linear(self.nHidden, self.nHidden)
-        self.linear3 = nn.Linear(self.nHidden, self.nHidden)
-        self.linear4 = nn.Linear(self.nHidden, self.nHidden)
-        self.linear5 = nn.Linear(self.nHidden, self.nHidden)
-        self.linear6 = nn.Linear(self.nHidden, self.nOutput)
-        # self.linear7 = nn.Linear(self.nHidden, self.nHidden)
-        # self.linear8 = nn.Linear(self.nHidden, self.nOutput)
-        self.ReLU    = nn.ReLU()
-
-    def forward(self, x):
-        h1 = self.ReLU(self.linear1(x))
-        h2 = self.ReLU(self.linear2(h1))
-        h3 = self.ReLU(self.linear3(h2))
-        h4 = self.ReLU(self.linear4(h3))
-        h5 = self.ReLU(self.linear5(h4))
-        # h6 = self.ReLU(self.linear6(h5))
-        # h7 = self.ReLU(self.linear7(h5))
-        output = self.linear6(h5) ## <- NOTE
-        return(output)
-
-mlpExplicit_3 = MLPexplicit_3(nInput, nHidden, nOutput)
-# which model to use from here onwards
-model_3 = mlpExplicit_3.to(device)
-'''
-
-##################################################
 ## network 4 dimension parameters
 ##################################################
 
@@ -277,12 +135,9 @@ train_dataloader = DataLoader(d, batch_size=25 , shuffle=True)
 
 # Define the loss function and optimizer
 loss_function = nn.MSELoss()
-# optimizer_1 = torch.optim.Adam(model_1.parameters(), lr=1e-4)
-# optimizer_2 = torch.optim.Adam(model_2.parameters(), lr=1e-4)
-# optimizer_3 = torch.optim.Adam(model_3.parameters(), lr=1e-4)
 optimizer_4 = torch.optim.Adam(model_4.parameters(), lr=1e-4)
-nTrainSteps = 1000
-nViewSteps = 50
+nTrainSteps = 100
+nViewSteps = 10
 nSize = nTrainSteps/nViewSteps
 e = []
 l_1 = []
@@ -309,35 +164,23 @@ for epoch in range(0, nTrainSteps):
     # Get inputs
     inputs, targets = data[0].to(device), data[1].to(device)
     # Zero the gradients
-    # optimizer_1.zero_grad()
-    # optimizer_2.zero_grad()
-    # optimizer_3.zero_grad()
     optimizer_4.zero_grad()
+
     # Perform forward pass (make sure to supply the input in the right way)
-    # outputs_1 = model_1(torch.reshape(inputs, (len(inputs), 1))).squeeze()
-    # outputs_2 = model_2(torch.reshape(inputs, (len(inputs), 1))).squeeze()
-    # outputs_3 = model_3(torch.reshape(inputs, (len(inputs), 1))).squeeze()
     outputs_4 = model_4(torch.reshape(inputs, (len(inputs), 1))).squeeze()
+
     # Compute loss
-    # loss_1 = loss_function(outputs_1, targets)
-    # loss_2 = loss_function(outputs_2, targets)
-    # loss_3 = loss_function(outputs_3, targets)
     loss_4 = loss_function(outputs_4, targets)
+
     # Perform backward pass
-    # loss_1.backward()
-    # loss_2.backward()
-    # loss_3.backward()
     loss_4.backward()
+
     # Perform optimization
-    # optimizer_1.step()
-    # optimizer_2.step()
-    # optimizer_3.step()
     optimizer_4.step()
+
     # Print statistics
-    # current_loss_1 += loss_1.item()
-    # current_loss_2 += loss_2.item()
-    # current_loss_3 += loss_3.item()
     current_loss_4 += loss_4.item()
+    l_4.append(current_loss_4)
 
     grad_total = 0.0
     r = 0.0
@@ -353,68 +196,43 @@ for epoch in range(0, nTrainSteps):
     grad_norm=grad_total**0.5
     grad_plot.append(grad_norm)
 
+    '''
     I = torch.func.hessian(loss_function)(outputs_4, targets)
-    print(f"I: {I}")
     L = torch.linalg.eigvals(I)
     for m in L:
         if m.real > 0.0001:
             r += 1
     ratio = r/len(np.array(torch.Tensor.cpu(L).detach().numpy()))
     min_ratio.append(ratio)
-
-  e.append(epoch+1)
-  # l_1.append(current_loss_1)
-  # l_2.append(current_loss_2)
-  # l_3.append(current_loss_3)
-  l_4.append(current_loss_4)
+    '''
+    
+  current_loss_4 = 0.0
 
   if (epoch + 1) % nViewSteps == 0:
-      '''print('Loss_1 after epoch %5d: %.3f' %
-            (epoch + 1, current_loss_1))
-      print('Loss_2 after epoch %5d: %.3f' %
-            (epoch + 1, current_loss_2))
-      print('Loss_3 after epoch %5d: %.3f' %
-            (epoch + 1, current_loss_3))'''
       print('Loss_4 after epoch %5d: %.3f' %
             (epoch + 1, current_loss_4))
-      current_loss_1 = 0.0
-      current_loss_2 = 0.0
-      current_loss_3 = 0.0
-      current_loss_4 = 0.0
+      
 
 # Process is complete.
 print('Training process has finished.')
 
-print(f"Min Ratio: {min_ratio}")
+# print(f"Min Ratio: {min_ratio}")
 
-# yPred_1 = np.array([torch.Tensor.cpu(model_1.forward(torch.tensor([o]).to(device))).detach().numpy() for o in xObs]).flatten()
-# yPred_2 = np.array([torch.Tensor.cpu(model_2.forward(torch.tensor([o]).to(device))).detach().numpy() for o in xObs]).flatten()
-# yPred_3 = np.array([torch.Tensor.cpu(model_3.forward(torch.tensor([o]).to(device))).detach().numpy() for o in xObs]).flatten()
 yPred_4 = np.array([torch.Tensor.cpu(model_4.forward(torch.tensor([o]).to(device))).detach().numpy() for o in xObs]).flatten()
 
 # plot the data
-plt.figure()
-# plt.plot(e, l_1, color="r", alpha= 1.0, label= "Plt_1")
-# plt.plot(e, l_2, color="b", alpha= 1.0, label= "Plt_2")
-# plt.plot(e, l_3, color="y", alpha= 1.0, label= "Plt_3")
-plt.plot(e, l_4, color="g", alpha= 1.0, label= "Plt_4")
-plt.yscale('log')
+fig = plt.figure()
+ax1 = plt.subplot(2,1,1)
+ax1.plot(range(len(grad_plot)), grad_plot, color="r")
+ax1.set_xlabel('Steps')
+ax1.set_ylabel('Gradient')
 
-plt.figure()
-d = pd.DataFrame({'xObs' : xObs.detach().numpy(),
-                  'yObs' : yObs.detach().numpy(),
-                  # 'yPred_1': yPred_1,
-                  # 'yPred_2': yPred_2,
-                  # 'yPred_3': yPred_3,
-                  'yPred_4': yPred_4})
+ax2 = plt.subplot(2,1,2)
+ax2.plot(range(len(l_4)), l_4, color="b")
+ax2.set_xlabel('Steps')
+ax2.set_ylabel('Loss')
 
-dWide = pd.melt(d, id_vars = 'xObs', value_vars= ['yObs', 'yPred_4'])
-sns.scatterplot(data = dWide, x = 'xObs', y = 'value', hue = 'variable', alpha = 0.7)
-x = np.linspace(start = 0, stop = 1, num = 1000)
-y = goalFun(x)
-plt.plot(x,y, color='g', alpha = 0.5)
-
-plt.figure()
-plt.plot(range(len(grad_plot)), grad_plot, color="r", alpha= 1.0, label= "Plt_1")
+# sin(5*np.pi*x)/(5*np.pi*x)
+fig.suptitle(r"$\ y = \frac{\sin{5*\pi*x}}{5*\pi*x}$")
 
 plt.show()
--- PCA_Analysis.py
+++ PCA_Analysis.py
@@ -142,7 +142,9 @@ def test(dataloader, model, loss_fn):
     l_1.append(test_loss)
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-epochs = 10
+# Train 8 times
+epochs = 8
+
 dataTensor_1 = torch.empty((1, 1024), dtype=torch.float32).fill_(0)
 dataTensor_2 = torch.empty((1, 1024), dtype=torch.float32).fill_(0)
 dataTensor_3 = torch.empty((1, 1024), dtype=torch.float32).fill_(0)
@@ -152,9 +154,10 @@ for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     epoch.append(t+1)
     train(train_dataloader, model, loss_fn, optimizer)
-    train(train_dataloader, model, loss_fn, optimizer)
 
-    ###################################################
+    ##########################################################
+    # Run PCA analysis on modified model weights for Layer 1 #
+    ##########################################################
     m_1 = model.linear1
     print(f"Model Data Shape: {m_1.weight.data.shape}")
     wData_1 = torch.sum(m_1.weight.data,1)[:,None]
@@ -162,72 +165,81 @@ for t in range(epochs):
     dataTensor_1 = torch.cat((dataTensor_1, torch.Tensor.cpu(wData_1)), 0)
     print(f"wData_1: {wData_1.shape}")
     print(f"wData_1: {wData_1}")
-    if (t+1) % 2 == 0:
+    if (t+1) % 3 == 0:
         principal.fit(torch.Tensor.cpu(dataTensor_1[1:]))
         weight_1.append(np.array(principal.transform(torch.Tensor.cpu(dataTensor_1[1:]))))
         print(f"Weight Values: {weight_1}")
         del dataTensor_1
         dataTensor_1 = torch.empty((1, 1024), dtype=torch.float32).fill_(0)
-    ###################################################
+
+    ##########################################################
+    # Run PCA analysis on modified model weights for Layer 2 #
+    ##########################################################
     m_2 = model.linear2
     wData_2 = torch.sum(m_2.weight.data,1)[:, None]
     wData_2 = torch.transpose(wData_2, 0, 1)
     dataTensor_2 = torch.cat((dataTensor_2, torch.Tensor.cpu(wData_2)), 0)
     print(f"wData_2 Shape: {dataTensor_2.shape}")
-    if (t+1) % 2 == 0:
+    if (t+1) % 3 == 0:
         principal_2.fit(torch.Tensor.cpu(dataTensor_2[1:]))
         weight_2.append(np.array(principal_2.transform(torch.Tensor.cpu(dataTensor_2[1:]))))
         del dataTensor_2
         dataTensor_2 = torch.empty((1, 1024), dtype=torch.float32).fill_(0)
-    ###################################################
+
+    ##########################################################
+    # Run PCA analysis on modified model weights for Layer 3 #
+    ##########################################################
     m_3 = model.linear3
     wData_3 = torch.sum(m_3.weight.data,1)[:, None]
     wData_3 = torch.transpose(wData_3, 0, 1)
     dataTensor_3 = torch.cat((dataTensor_3, torch.Tensor.cpu(wData_3)), 0)
     print(f"wData_3 Shape: {dataTensor_3.shape}")
-    if (t+1) % 2 == 0:
+    if (t+1) % 3 == 0:
         principal_3.fit(torch.Tensor.cpu(dataTensor_3[1:]))
         weight_3.append(np.array(principal_3.transform(torch.Tensor.cpu(dataTensor_3[1:]))))
         del dataTensor_3
         dataTensor_3 = torch.empty((1, 1024), dtype=torch.float32).fill_(0)
-    ###################################################
+
+    ##########################################################
+    # Run PCA analysis on modified model weights for Layer 4 #
+    ##########################################################
     m_4 = model.linear4
     wData_4 = torch.sum(m_4.weight.data,1)[:, None]
     wData_4 = torch.transpose(wData_4, 0, 1)
     dataTensor_4 = torch.cat((dataTensor_4, torch.Tensor.cpu(wData_4)), 0)
     print(f"wData_4 Shape: {dataTensor_4.shape}")
-    if (t+1) % 2 == 0:
+    if (t+1) % 3 == 0:
         principal_4.fit(torch.Tensor.cpu(dataTensor_4[1:]))
         weight_4.append(np.array(principal_4.transform(torch.Tensor.cpu(dataTensor_4[1:]))))
         del dataTensor_4
         dataTensor_4 = torch.empty((1, 10), dtype=torch.float32).fill_(0)
-    ###################################################
 
+    # Run Test
     test(test_dataloader, model, loss_fn)
 print("Done!")
 
-# plot the data
-# plt.figure()
-# plt.plot(epoch, l_1, color="r", alpha= 1.0, label= "Plt_1")
-# plt.yscale('log')
-
 fig, ax = plt.subplots()
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+
+fig.suptitle('PCA Analysis @ Model Level')
 
 x_1 = [x[0] for x in weight_1]
 y_1 = [y[1] for y in weight_1]
-ax.scatter(x_1,y_1, c='tab:blue', label='L_1')
+ax.scatter(x_1,y_1, c='tab:blue', label='Linear Layer 1')
 
 x_2 = [x[0] for x in weight_2]
 y_2 = [y[1] for y in weight_2]
-ax.scatter(x_2,y_2, c='tab:green', label='L_2')
+ax.scatter(x_2,y_2, c='tab:green', label='linear Layer 2')
 
 x_3 = [x[0] for x in weight_3]
 y_3 = [y[1] for y in weight_3]
-ax.scatter(x_3,y_3, c='tab:red', label='L_3')
+ax.scatter(x_3,y_3, c='tab:red', label='Linear Layer 3')
 
 x_4 = [x[0] for x in weight_4]
 y_4 = [y[1] for y in weight_4]
-ax.scatter(x_4,y_4, c='tab:orange', label='L_4')
+ax.scatter(x_4,y_4, c='tab:orange', label='Linear Layer 4')
 
 ax.legend()
 
